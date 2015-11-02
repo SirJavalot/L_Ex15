33b6c6679a2aadb1bddff967527a3b404691f348
SirJavalot <dennis155@hotmail.com>
2015-11-02 13:02:12 +0000

    second test

--- src/EX15_01.java
+++ src/EX15_01.java
@@ -5,14 +5,13 @@ import javafx.stage.Stage;
 public class EX15_01 extends Application {
 
 	@Override
-	public void start(Stage arg0) throws Exception {
+	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Application.launch(args);
 	}
 
 }
